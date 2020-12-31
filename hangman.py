eaa219f39e49033a699f7dfc06b46327e2a84fe9
Hong Dah Ong <40519064+ezpher@users.noreply.github.com>
2021-01-01 01:12:50 +0800

    remove redundant print statements

--- hangman.py
+++ hangman.py
@@ -20,54 +20,43 @@ def play(word):
 
     print()
     print("Let's play hangman: ")
-    print(f'You have {tries} tries: ')
-    print(display_hangman(tries))
 
     while len(word_letters) > 0 and tries > 0:
 
         guessed_word = [letter if letter in used_letters else '-' for letter in word]
 
         print()
+        print(display_hangman(tries))
+        print(f'Tries left: {tries}')
         print('Guessed Word: ', ' '.join(guessed_word))
         print('Used Letters: ', ' '.join(used_letters))
 
         guessed_letter = input('Guess a letter: ').upper()
 
+        print()
         if guessed_letter in alphabet_letters and guessed_letter not in used_letters:
             used_letters.append(guessed_letter)
 
             if guessed_letter in word_letters:
                 word_letters.remove(guessed_letter)
-
-                print()
                 print(f'The letter {guessed_letter} has been guessed correctly')
             else:
-                print()
                 print('Try again. The letter is not in the word.')
-
                 tries -= 1
-                print(f'Tries left: {tries}')
 
         elif guessed_letter in used_letters:
-            print()
             print('Letter has been used')
 
         else:
-            print()
             print('Not a valid letter')
-
             tries -= 1
-            print(f'Tries left: {tries}')
-
-        print()
-        print(display_hangman(tries))
 
     print()
     if tries == 0:
         print('You have no more tries left')
-        print(f"The word is {word}")
     else:
         print('You have guessed the word correctly!')
+    print(f"The word is {word}")
 
 def display_hangman(tries):
 
